e4ee9eae595ed52dbaa17343984d9d798cec9e39
rik1599 <rik1599@github.com>
2020-09-12 22:22:30 +0200

    Primi esperimenti

---
+++ src/Controller/Student/StudentDashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Student;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentDashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/student", name="student")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle("ECDL Dashboard\nStudente");
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+    }
+}
